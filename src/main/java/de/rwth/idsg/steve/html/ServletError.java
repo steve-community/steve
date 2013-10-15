adca3cd46f1d7bedcb7e4961c362aa52079e865f
goekay <goekay@idsg>
2013-10-15 10:44:49 +0200

    javadoc update

--- src/main/java/de/rwth/idsg/steve/html/ServletError.java
+++ src/main/java/de/rwth/idsg/steve/html/ServletError.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+* 
+* @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
+* 
+*/
 public class ServletError extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;

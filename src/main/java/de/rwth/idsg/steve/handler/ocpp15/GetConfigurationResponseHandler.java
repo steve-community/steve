b8fd3b95bc6dd86df95be536aa096418eef7fcec
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2015-04-30 20:07:28 +0200

    fix: print the GetConfigurationResponse nicely (inline HTML is bad practice, but whatever)

--- src/main/java/de/rwth/idsg/steve/handler/ocpp15/GetConfigurationResponseHandler.java
+++ src/main/java/de/rwth/idsg/steve/handler/ocpp15/GetConfigurationResponseHandler.java
@@ -1,8 +1,12 @@
 package de.rwth.idsg.steve.handler.ocpp15;
 
+import com.google.common.base.Joiner;
 import de.rwth.idsg.steve.handler.AbstractOcppResponseHandler;
 import de.rwth.idsg.steve.web.RequestTask;
 import ocpp.cp._2012._06.GetConfigurationResponse;
+import ocpp.cp._2012._06.KeyValue;
+
+import java.util.List;
 
 /**
  * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
@@ -10,18 +14,51 @@ import ocpp.cp._2012._06.GetConfigurationResponse;
  */
 public class GetConfigurationResponseHandler extends AbstractOcppResponseHandler<GetConfigurationResponse> {
 
+    private static final String FORMAT =
+            "<b>Known keys:</b>" +
+            "<br>" +
+            "%s" +
+            "<br>" +
+            "<b>Unknown keys:</b>" +
+            "<br>" +
+            "%s";
+
+    private static final Joiner JOINER = Joiner.on(",");
+
     public GetConfigurationResponseHandler(RequestTask requestTask, String chargeBoxId) {
         super(requestTask, chargeBoxId);
     }
 
     @Override
     public void handleResult(GetConfigurationResponse response) {
-        // TODO: Not sure whether this prints nicely
-        String str = "Known keys: "
-                   + response.getConfigurationKey().toString()
-                   + " / Unknown keys: "
-                   + response.getUnknownKey().toString();
+        String str = String.format(
+                FORMAT,
+                toStringConfList(response.getConfigurationKey()),
+                toStringUnknownList(response.getUnknownKey())
+        );
 
         requestTask.addNewResponse(chargeBoxId, str);
+    }
+
+    private String toStringConfList(List<KeyValue> confList) {
+        StringBuilder sb = new StringBuilder();
+
+        for (KeyValue keyValue : confList) {
+            sb.append(keyValue.getKey())
+              .append(": ")
+              .append(keyValue.getValue());
+
+            if (keyValue.isReadonly()) {
+                sb.append(" (read-only)");
+            }
+
+            sb.append("<br>");
+        }
+
+        return sb.toString();
+    }
+
+    private String toStringUnknownList(List<String> unknownList) {
+        return JOINER.join(unknownList);
     }
 }

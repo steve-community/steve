069029cd501553fd0a59ece9c297b6dc5d35cfb5
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2015-10-24 15:17:42 +0200

    disable defaultSockJsTaskScheduler [ci skip]

--- src/main/java/de/rwth/idsg/steve/config/WebSocketConfiguration.java
+++ src/main/java/de/rwth/idsg/steve/config/WebSocketConfiguration.java
@@ -18,7 +18,10 @@ import org.eclipse.jetty.websocket.api.WebSocketPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.ServletWebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
@@ -82,5 +85,28 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
                 )
         );
         return mapper;
+    }
+
+    // -------------------------------------------------------------------------
+    // We don't need no SockJS fallback. But, nevertheless, Spring initializes
+    // a scheduler for it, to be used in the implementation
+    // AbstractWebSocketHandlerRegistration.withSockJS()}. We don't call
+    // WebSocketHandlerRegistry.withSockJS() above to add SockJS support,
+    // so the scheduler is useless.
+    //
+    // Hereby, we override the default beans provided by
+    // org.springframework.web.socket.config.annotation.WebSocketConfigurationSupport
+    // -------------------------------------------------------------------------
+
+    @Bean
+    public HandlerMapping webSocketHandlerMapping() {
+        ServletWebSocketHandlerRegistry registry = new ServletWebSocketHandlerRegistry(null);
+        registerWebSocketHandlers(registry);
+        return registry.getHandlerMapping();
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler defaultSockJsTaskScheduler() {
+        return null;
     }
 }

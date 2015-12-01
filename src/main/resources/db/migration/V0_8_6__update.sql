fe90b9ffbc6cc9e46990074e78f8e050a6188d97
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2015-12-01 13:42:27 +0100

    revert back to utf8 due to indexing issues, even though it's inferior
    problem: "Specified key was too long; max key length is 767 bytes (SQL State: 42000 - Error Code: 1071)"
    
    more info:
    - https://dev.mysql.com/doc/refman/5.7/en/charset-unicode-upgrading.html
    - https://mathiasbynens.be/notes/mysql-utf8mb4
    - https://make.wordpress.org/core/2015/04/02/the-utf8mb4-upgrade/

--- src/main/resources/db/migration/V0_8_6__update.sql
+++ src/main/resources/db/migration/V0_8_6__update.sql
@@ -8,9 +8,6 @@
 -- and also we cannot touch book keeping table "schema_version" of flyway,
 -- since it will be in use during the migration.
 --
--- https://dev.mysql.com/doc/refman/5.5/en/charset-unicode-utf8mb4.html
--- http://stackoverflow.com/a/766996
---
 
 LOCK TABLES
   address WRITE,
@@ -26,17 +23,41 @@ LOCK TABLES
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-ALTER TABLE address CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE charge_box CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE connector CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE connector_meter_value CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE connector_status CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE ocpp_tag CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE reservation CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE settings CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `transaction` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `user` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE charge_box CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE address CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE connector CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE connector_meter_value CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE connector_status CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE ocpp_tag CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE reservation CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE settings CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `transaction` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `user` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
 UNLOCK TABLES;
+
+REPAIR TABLE
+  address,
+  charge_box,
+  connector,
+  connector_meter_value,
+  connector_status,
+  ocpp_tag,
+  reservation,
+  settings,
+  `transaction`,
+  `user`;
+
+OPTIMIZE TABLE
+  address,
+  charge_box,
+  connector,
+  connector_meter_value,
+  connector_status,
+  ocpp_tag,
+  reservation,
+  settings,
+  `transaction`,
+  `user`;

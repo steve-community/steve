7a6aaf9723b6136a5b750f274b1a9b43aecb51d2
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2016-03-17 17:54:13 +0100

    disable ConcurrentTx check
    this causes more harm than good. example use case: if the expiry date is set such, that the local cache is invalidated before stop transaction and the charging station sends an authorize in order to stop a transaction, we will send a "ConcurrentTx" for this rfid. Instead of thinking "oh this might be due to the ongoing transaction that the user wants to stop", the charging station might stop further processing. because of this the process breaks and the transaction cannot be stopped.

--- src/main/java/de/rwth/idsg/steve/service/OcppTagServiceImpl.java
+++ src/main/java/de/rwth/idsg/steve/service/OcppTagServiceImpl.java
@@ -49,13 +49,13 @@ public class OcppTagServiceImpl implements OcppTagService {
             log.error("The user with idTag '{}' is INVALID (not present in DB).", idTag);
             idTagInfo.setStatus(AuthorizationStatus.INVALID);
         } else {
-            if (record.getInTransaction()) {
-                log.warn("The user with idTag '{}' is ALREADY in another transaction.", idTag);
-                idTagInfo.setStatus(AuthorizationStatus.CONCURRENT_TX);
-
-            } else if (record.getBlocked()) {
+            if (record.getBlocked()) {
                 log.error("The user with idTag '{}' is BLOCKED.", idTag);
                 idTagInfo.setStatus(AuthorizationStatus.BLOCKED);
+
+//            } else if (record.getInTransaction()) {
+//                log.warn("The user with idTag '{}' is ALREADY in another transaction.", idTag);
+//                idTagInfo.setStatus(AuthorizationStatus.CONCURRENT_TX);
 
             } else if (record.getExpiryDate() != null && DateTime.now().isAfter(record.getExpiryDate())) {
                 log.error("The user with idTag '{}' is EXPIRED.", idTag);
@@ -82,13 +82,13 @@ public class OcppTagServiceImpl implements OcppTagService {
             log.error("The user with idTag '{}' is INVALID (not present in DB).", idTag);
             idTagInfo.setStatus(ocpp.cs._2012._06.AuthorizationStatus.INVALID);
         } else {
-            if (record.getInTransaction()) {
-                log.warn("The user with idTag '{}' is ALREADY in another transaction.", idTag);
-                idTagInfo.setStatus(ocpp.cs._2012._06.AuthorizationStatus.CONCURRENT_TX);
-
-            } else if (record.getBlocked()) {
+            if (record.getBlocked()) {
                 log.error("The user with idTag '{}' is BLOCKED.", idTag);
                 idTagInfo.setStatus(ocpp.cs._2012._06.AuthorizationStatus.BLOCKED);
+
+//            } else if (record.getInTransaction()) {
+//                log.warn("The user with idTag '{}' is ALREADY in another transaction.", idTag);
+//                idTagInfo.setStatus(ocpp.cs._2012._06.AuthorizationStatus.CONCURRENT_TX);
 
             } else if (record.getExpiryDate() != null && DateTime.now().isAfter(record.getExpiryDate())) {
                 log.error("The user with idTag '{}' is EXPIRED.", idTag);

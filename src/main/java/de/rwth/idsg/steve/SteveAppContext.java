9494ab2d4653e3d0c77201503c02ddc297495082
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2016-04-21 10:42:06 +0200

    do away with jsp pre-compilation
    preparing for configurable context path (i.e. changing the prefix /steve)

--- src/main/java/de/rwth/idsg/steve/SteveAppContext.java
+++ src/main/java/de/rwth/idsg/steve/SteveAppContext.java
@@ -24,6 +24,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.EnumSet;
@@ -141,9 +142,31 @@ public class SteveAppContext {
     // -------------------------------------------------------------------------
 
     private void initJSP(WebAppContext ctx) throws IOException {
+        ctx.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/[^/]*servlet-api-[^/]*\\.jar$"
+                        + "|.*/javax.servlet.jsp.jstl-.*\\.jar$"
+                        + "|.*/spring-webmvc.*\\.jar$"
+                        + "|.*/.*taglibs.*\\.jar$");
+
+        ctx.setAttribute("javax.servlet.context.tempdir", getScratchDir());
         ctx.setAttribute("org.eclipse.jetty.containerInitializers", jspInitializers());
         ctx.setAttribute(InstanceManager.class.getName(), new SimpleInstanceManager());
         ctx.addBean(new ServletContainerInitializersStarter(ctx), true);
+    }
+
+    /**
+     * Establish Scratch directory for the servlet context (used by JSP compilation)
+     */
+    private File getScratchDir() throws IOException {
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(), "embedded-jetty-jsp");
+
+        if (!scratchDir.exists()) {
+            if (!scratchDir.mkdirs()) {
+                throw new IOException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
+        return scratchDir;
     }
 
     /**

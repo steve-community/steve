351f0ae4c7ad8088bd7c83288062d06e5df62c75
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2016-07-26 13:52:28 +0200

    migrate to callback-style for connect/disconnect actions

--- src/main/java/de/rwth/idsg/steve/ocpp/ws/AbstractWebSocketEndpoint.java
+++ src/main/java/de/rwth/idsg/steve/ocpp/ws/AbstractWebSocketEndpoint.java
@@ -19,12 +19,14 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
@@ -42,11 +44,17 @@ public abstract class AbstractWebSocketEndpoint implements WebSocketHandler {
     public static final String CHARGEBOX_ID_KEY = "CHARGEBOX_ID_KEY";
 
     private Pipeline pipeline;
-    protected SessionContextStoreImpl sessionContextStore;
+    private SessionContextStoreImpl sessionContextStore;
+
+    private final List<Consumer<String>> connectedCallbackList = new ArrayList<>();
+    private final List<Consumer<String>> disconnectedCallbackList = new ArrayList<>();
 
     public void init(Pipeline pipeline) {
         this.pipeline = pipeline;
         sessionContextStore = new SessionContextStoreImpl(wsSessionSelectStrategy);
+
+        connectedCallbackList.add((chargeBoxId) -> notificationService.ocppStationWebSocketConnected(chargeBoxId));
+        disconnectedCallbackList.add((chargeBoxId) -> notificationService.ocppStationWebSocketDisconnected(chargeBoxId));
     }
 
     @Override
@@ -108,7 +116,7 @@ public abstract class AbstractWebSocketEndpoint implements WebSocketHandler {
         // Take into account that there might be multiple connections to a charging station.
         // Send notification only for the change 0 -> 1.
         if (sizeBeforeAdd == 0) {
-            notificationService.ocppStationWebSocketConnected(chargeBoxId);
+            connectedCallbackList.forEach(consumer -> consumer.accept(chargeBoxId));
         }
     }
 
@@ -125,7 +133,7 @@ public abstract class AbstractWebSocketEndpoint implements WebSocketHandler {
         // Take into account that there might be multiple connections to a charging station.
         // Send notification only for the change 1 -> 0.
         if (sizeAfterRemove == 0) {
-            notificationService.ocppStationWebSocketDisconnected(chargeBoxId);
+            disconnectedCallbackList.forEach(consumer -> consumer.accept(chargeBoxId));
         }
     }
 
@@ -146,6 +154,14 @@ public abstract class AbstractWebSocketEndpoint implements WebSocketHandler {
 
     protected String getChargeBoxId(WebSocketSession session) {
         return (String) session.getAttributes().get(CHARGEBOX_ID_KEY);
+    }
+
+    protected void registerConnectedCallback(Consumer<String> consumer) {
+        connectedCallbackList.add(consumer);
+    }
+
+    protected void registerDisconnectedCallback(Consumer<String> consumer) {
+        disconnectedCallbackList.add(consumer);
     }
 
     public List<String> getChargeBoxIdList() {

00083d13d83f7f0ea017aa32c8cf7596ea765ccc
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2018-03-14 14:00:03 +0100

    use ocpp 1.6 connector status values after transaction start/stop

--- src/main/java/de/rwth/idsg/steve/repository/dto/TransactionStatusUpdate.java
+++ src/main/java/de/rwth/idsg/steve/repository/dto/TransactionStatusUpdate.java
@@ -1,23 +1,24 @@
 package de.rwth.idsg.steve.repository.dto;
 
 import lombok.Getter;
-import ocpp.cs._2012._06.ChargePointErrorCode;
-import ocpp.cs._2012._06.ChargePointStatus;
-
-import static ocpp.cs._2012._06.ChargePointStatus.AVAILABLE;
-import static ocpp.cs._2012._06.ChargePointStatus.OCCUPIED;
+import ocpp.cs._2015._10.ChargePointErrorCode;
+import ocpp.cs._2015._10.ChargePointStatus;
 
 /**
- * 1) Exists only to ensure type safety
- * 2) Does not matter, whether we use ChargePointStatus and ChargePointErrorCode from OCPP 1.2 or 1.5
+ * Exists only to ensure type safety
  *
  * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
  * @since 17.03.2016
  */
 @Getter
 public enum TransactionStatusUpdate {
-    AfterStart(OCCUPIED),
-    AfterStop(AVAILABLE);
+
+    // From OCPP 1.6: When the contactor of a Connector closes, allowing the vehicle to charge
+    AfterStart(ChargePointStatus.CHARGING),
+
+    // From OCPP 1.6: When a charging session has stopped at a Connector, but the Connector is not yet available
+    // for a new user, e.g. the cable has not been removed or the vehicle has not left the parking bay
+    AfterStop(ChargePointStatus.FINISHING);
 
     private final String status;
     private final String errorCode = ChargePointErrorCode.NO_ERROR.value();

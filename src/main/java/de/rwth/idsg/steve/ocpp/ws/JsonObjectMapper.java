69eb2fc00264bb454611b4806ae57cc71cce4b4f
Andreas Heuvels <andreas.heuvels@rwth-aachen.de>
2018-04-17 15:41:12 +0200

    Json parsing: fail on null for primitives

--- src/main/java/de/rwth/idsg/steve/ocpp/ws/JsonObjectMapper.java
+++ src/main/java/de/rwth/idsg/steve/ocpp/ws/JsonObjectMapper.java
@@ -9,6 +9,8 @@ import de.rwth.idsg.steve.ocpp.ws.ocpp12.Ocpp12JacksonModule;
 import de.rwth.idsg.steve.ocpp.ws.ocpp15.Ocpp15JacksonModule;
 import de.rwth.idsg.steve.ocpp.ws.ocpp16.Ocpp16JacksonModule;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES;
+
 /**
  * Because ObjectMapper can and should be reused, if config does not change after init.
  *
@@ -23,6 +25,15 @@ public enum JsonObjectMapper {
     JsonObjectMapper() {
         mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        // OCPP messages contain some mandatory primitive fields (like transactionId), that are not allowed
+        // to be null. any misinterpretation/mapping of these fields like "null -> 0" is a mistake.
+        //
+        // true story: while testing with abusive-charge-point, it sends stopTransactions where transactionId=null
+        // in communication flows, where a startTransaction before causes an Exception and we cannot send a regular
+        // response with a transactionId, but an error message. if we do not fail early, it will fail at the database
+        // level which we want to prevent.
+        mapper.configure(FAIL_ON_NULL_FOR_PRIMITIVES, true);
 
         mapper.registerModule(new Ocpp12JacksonModule());
         mapper.registerModule(new Ocpp15JacksonModule());

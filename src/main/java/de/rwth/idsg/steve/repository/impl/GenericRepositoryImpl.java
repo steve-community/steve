3c8b19e1685d25d23650fe92ed80b8ba702ea16b
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2018-05-30 16:34:12 +0200

    move the logic of stored proc "get_stats" to java
    reason: to make the future transition to mysql 8 easier
    (see issue #67)

--- src/main/java/de/rwth/idsg/steve/repository/impl/GenericRepositoryImpl.java
+++ src/main/java/de/rwth/idsg/steve/repository/impl/GenericRepositoryImpl.java
@@ -1,18 +1,26 @@
 package de.rwth.idsg.steve.repository.impl;
 
 import de.rwth.idsg.steve.repository.GenericRepository;
+import de.rwth.idsg.steve.repository.ReservationStatus;
 import de.rwth.idsg.steve.repository.dto.DbVersion;
 import de.rwth.idsg.steve.utils.DateTimeUtils;
 import de.rwth.idsg.steve.web.dto.Statistics;
-import jooq.steve.db.routines.GetStats;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 import org.jooq.DSLContext;
+import org.jooq.Field;
 import org.jooq.Record2;
+import org.jooq.Record8;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import static de.rwth.idsg.steve.utils.CustomDSL.date;
+import static jooq.steve.db.Tables.RESERVATION;
+import static jooq.steve.db.Tables.TRANSACTION;
+import static jooq.steve.db.tables.ChargeBox.CHARGE_BOX;
+import static jooq.steve.db.tables.OcppTag.OCPP_TAG;
 import static jooq.steve.db.tables.SchemaVersion.SCHEMA_VERSION;
+import static jooq.steve.db.tables.User.USER;
 import static org.jooq.impl.DSL.max;
 import static org.jooq.impl.DSL.select;
 
@@ -28,20 +36,76 @@ public class GenericRepositoryImpl implements GenericRepository {
 
     @Override
     public Statistics getStats() {
+        DateTime now = DateTime.now();
+        DateTime yesterdaysNow = now.minusDays(1);
 
-        // getStats is the stored procedure in our MySQL DB
-        GetStats gs = new GetStats();
-        gs.execute(ctx.configuration());
+        Field<Integer> numChargeBoxes =
+                ctx.selectCount()
+                   .from(CHARGE_BOX)
+                   .asField("num_charge_boxes");
+
+        Field<Integer> numOcppTags =
+                ctx.selectCount()
+                   .from(OCPP_TAG)
+                   .asField("num_ocpp_tags");
+
+        Field<Integer> numUsers =
+                ctx.selectCount()
+                   .from(USER)
+                   .asField("num_users");
+
+        Field<Integer> numReservations =
+                ctx.selectCount()
+                   .from(RESERVATION)
+                   .where(RESERVATION.EXPIRY_DATETIME.greaterThan(now))
+                   .and(RESERVATION.STATUS.eq(ReservationStatus.ACCEPTED.name()))
+                   .asField("num_reservations");
+
+        Field<Integer> numTransactions =
+                ctx.selectCount()
+                   .from(TRANSACTION)
+                   .where(TRANSACTION.STOP_TIMESTAMP.isNull())
+                   .asField("num_transactions");
+
+        Field<Integer> heartbeatsToday =
+                ctx.selectCount()
+                   .from(CHARGE_BOX)
+                   .where(date(CHARGE_BOX.LAST_HEARTBEAT_TIMESTAMP).eq(date(now)))
+                   .asField("heartbeats_today");
+
+        Field<Integer> heartbeatsYesterday =
+                ctx.selectCount()
+                   .from(CHARGE_BOX)
+                   .where(date(CHARGE_BOX.LAST_HEARTBEAT_TIMESTAMP).eq(date(yesterdaysNow)))
+                   .asField("heartbeats_yesterday");
+
+        Field<Integer> heartbeatsEarlier =
+                ctx.selectCount()
+                   .from(CHARGE_BOX)
+                   .where(date(CHARGE_BOX.LAST_HEARTBEAT_TIMESTAMP).lessThan(date(yesterdaysNow)))
+                   .asField("heartbeats_earlier");
+
+        Record8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> gs =
+                ctx.select(
+                        numChargeBoxes,
+                        numOcppTags,
+                        numUsers,
+                        numReservations,
+                        numTransactions,
+                        heartbeatsToday,
+                        heartbeatsYesterday,
+                        heartbeatsEarlier
+                ).fetchOne();
 
         return Statistics.builder()
-                         .numChargeBoxes(gs.getNumChargeBoxes())
-                         .numOcppTags(gs.getNumOcppTags())
-                         .numUsers(gs.getNumUsers())
-                         .numReservations(gs.getNumReservations())
-                         .numTransactions(gs.getNumTransactions())
-                         .heartbeatToday(gs.getHeartbeatsToday())
-                         .heartbeatYesterday(gs.getHeartbeatsYesterday())
-                         .heartbeatEarlier(gs.getHeartbeatsEarlier())
+                         .numChargeBoxes(gs.value1())
+                         .numOcppTags(gs.value2())
+                         .numUsers(gs.value3())
+                         .numReservations(gs.value4())
+                         .numTransactions(gs.value5())
+                         .heartbeatToday(gs.value6())
+                         .heartbeatYesterday(gs.value7())
+                         .heartbeatEarlier(gs.value8())
                          .build();
     }
 
---
+++ src/main/resources/db/migration/V0_9_4__update.sql
@@ -0,0 +1,1 @@
+DROP PROCEDURE `get_stats`;

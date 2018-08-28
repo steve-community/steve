8ad7c836afef029da00a26b7761ec53c83163335
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2018-08-28 12:38:09 +0200

    fix ssl init

--- src/main/java/de/rwth/idsg/steve/ocpp/soap/ClientProvider.java
+++ src/main/java/de/rwth/idsg/steve/ocpp/soap/ClientProvider.java
@@ -1,5 +1,7 @@
 package de.rwth.idsg.steve.ocpp.soap;
 
+import com.oneandone.compositejks.CompositeX509KeyManager;
+import com.oneandone.compositejks.CompositeX509TrustManager;
 import com.oneandone.compositejks.KeyStoreLoader;
 import com.oneandone.compositejks.SslContextUtils;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
@@ -12,9 +14,14 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
 import javax.xml.ws.soap.SOAPBinding;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 
 import static de.rwth.idsg.steve.SteveConfiguration.CONFIG;
 
@@ -67,15 +74,35 @@ public class ClientProvider {
     private static SSLSocketFactory setupSSL() {
         SSLContext ssl;
         try {
-            ssl = SslContextUtils.buildMergedWithSystem(
-                    KeyStoreLoader.fromStream(
-                            CONFIG.getJetty().getKeyStorePath(),
-                            CONFIG.getJetty().getKeyStorePassword().toCharArray()
-                    )
-            );
+            String keyStorePath = CONFIG.getJetty().getKeyStorePath();
+            char[] keyStorePwd = CONFIG.getJetty().getKeyStorePassword().toCharArray();
+            KeyStore keyStore = KeyStoreLoader.fromStream(keyStorePath, keyStorePwd);
+            ssl = buildMergedWithSystem(keyStore, keyStorePwd);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return ssl.getSocketFactory();
+    }
+
+    /**
+     * Because {@link SslContextUtils#buildMergedWithSystem(java.security.KeyStore)} does not propagate the param for
+     * the password of the keystore and we got exceptions like "java.security.UnrecoverableKeyException: Password must not be null".
+     *
+     * While we are at it, we use the default algorithm instead of hard-coding 'SunX509'.
+     */
+    private static SSLContext buildMergedWithSystem(KeyStore keyStore, char[] keyStorePwd) throws GeneralSecurityException {
+        String defaultAlgorithm = KeyManagerFactory.getDefaultAlgorithm();
+
+        KeyManager[] keyManagers = {new CompositeX509KeyManager(
+                SslContextUtils.getSystemKeyManager(defaultAlgorithm, keyStore, keyStorePwd),
+                SslContextUtils.getSystemKeyManager(defaultAlgorithm, null, null))};
+
+        TrustManager[] trustManagers = {new CompositeX509TrustManager(
+                SslContextUtils.getSystemTrustManager(defaultAlgorithm, keyStore),
+                SslContextUtils.getSystemTrustManager(defaultAlgorithm, null))};
+
+        SSLContext context = SSLContext.getInstance("SSL");
+        context.init(keyManagers, trustManagers, null);
+        return context;
     }
 }

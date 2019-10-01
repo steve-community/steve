e8cfb0cdaa4ba3bf10befedcda59ee0864d3f6a0
Sevket Gökay <sevketgokay@gmail.com>
2019-10-02 01:59:16 +0200

    update SecurityConfiguration to reflect spring-security change

--- src/main/java/de/rwth/idsg/steve/config/SecurityConfiguration.java
+++ src/main/java/de/rwth/idsg/steve/config/SecurityConfiguration.java
@@ -58,7 +58,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web.ignoring()
            .antMatchers("/static/**")
            .antMatchers("/views/**");

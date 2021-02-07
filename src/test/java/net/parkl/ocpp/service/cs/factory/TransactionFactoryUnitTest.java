2573d2c41c9de448fbc401522b6be00deda77ce7
Bence Németh <bence.nemeth@parkl.net>
2021-02-07 14:32:25 +0100

    PAR-4048: TransactionFactoryUnitTest

---
+++ src/test/java/net/parkl/ocpp/service/cs/factory/TransactionFactoryUnitTest.java
@@ -0,0 +1,109 @@
+package net.parkl.ocpp.service.cs.factory;
+
+import de.rwth.idsg.steve.repository.dto.InsertTransactionParams;
+import de.rwth.idsg.steve.repository.dto.UpdateTransactionParams;
+import net.parkl.ocpp.entities.*;
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import static com.google.common.base.Throwables.getStackTraceAsString;
+import static net.parkl.ocpp.entities.TransactionStopEventActor.manual;
+import static net.parkl.ocpp.service.cs.factory.TransactionFactory.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TransactionFactoryUnitTest {
+    @Test
+    public void testCreateTransactionStop() {
+        UpdateTransactionParams params = mock(UpdateTransactionParams.class);
+        DateTime timestamp = new DateTime();
+        TransactionStopEventActor stopEventActor = manual;
+        String stopMeterValue = "2";
+        String stopReason = "reason";
+
+        when(params.getEventTimestamp()).thenReturn(timestamp);
+        when(params.getEventActor()).thenReturn(stopEventActor);
+        when(params.getStopTimestamp()).thenReturn(timestamp);
+        when(params.getStopMeterValue()).thenReturn(stopMeterValue);
+        when(params.getStopReason()).thenReturn(stopReason);
+
+        TransactionStop transactionStop = createTransactionStop(params);
+
+        assertThat(transactionStop)
+                .isNotNull()
+                .extracting("eventActor",
+                            "stopTimestamp",
+                            "stopValue",
+                            "stopReason")
+                .contains(stopEventActor,
+                          timestamp.toDate(),
+                          stopMeterValue,
+                          stopReason);
+    }
+
+    @Test
+    public void testCreateTransactionStopFailed() {
+        UpdateTransactionParams params = mock(UpdateTransactionParams.class);
+        DateTime timestamp = new DateTime();
+        TransactionStopEventActor stopEventActor = manual;
+        String stopMeterValue = "2";
+        String stopReason = "reason";
+        Exception exception = mock(Exception.class);
+
+        when(params.getEventTimestamp()).thenReturn(timestamp);
+        when(params.getEventActor()).thenReturn(stopEventActor);
+        when(params.getStopTimestamp()).thenReturn(timestamp);
+        when(params.getStopMeterValue()).thenReturn(stopMeterValue);
+        when(params.getStopReason()).thenReturn(stopReason);
+
+        TransactionStopFailed transactionStop = createTransactionStopFailed(params, exception);
+
+        assertThat(transactionStop)
+                .isNotNull()
+                .extracting("eventActor",
+                            "stopTimestamp",
+                            "stopValue",
+                            "stopReason",
+                            "failReason")
+                .contains(stopEventActor,
+                          timestamp.toDate(),
+                          stopMeterValue,
+                          stopReason,
+                          getStackTraceAsString(exception));
+    }
+
+    @Test
+    public void testCreateTransactionStopId() {
+        int transactionPk = 1;
+        DateTime date = new DateTime();
+
+        TransactionStopId transactionStopId = createTransactionStopId(transactionPk, date);
+
+        assertThat(transactionStopId)
+                .isNotNull()
+                .extracting("transactionPk", "eventTimestamp")
+                .containsExactly(transactionPk, date.toDate());
+    }
+
+    @Test
+    public void testCreateTransactionStart() {
+        Connector connector = mock(Connector.class);
+        InsertTransactionParams params = mock(InsertTransactionParams.class);
+        String ocppIdTag = "TAG";
+        DateTime timeStamp = new DateTime();
+        String startMeterValue = "1";
+
+        when(params.getIdTag()).thenReturn(ocppIdTag);
+        when(params.getStartTimestamp()).thenReturn(timeStamp);
+        when(params.getStartMeterValue()).thenReturn(startMeterValue);
+        when(params.getEventTimestamp()).thenReturn(timeStamp);
+
+        TransactionStart transactionStart = createTransactionStart(connector, params);
+
+        assertThat(transactionStart)
+                .isNotNull()
+                .extracting("connector", "ocppTag", "startTimestamp", "startValue", "eventTimestamp")
+                .contains(connector, ocppIdTag, timeStamp.toDate(), startMeterValue, timeStamp.toDate());
+    }
+}

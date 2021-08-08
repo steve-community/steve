a41bdbac97be5401bcdaff54d68da0199fc26098
Sevket Gökay <sevketgokay@gmail.com>
2021-08-08 23:27:12 +0200

    add missing license headers

--- src/test/java/de/rwth/idsg/steve/web/validation/IdTagValidatorTest.java
+++ src/test/java/de/rwth/idsg/steve/web/validation/IdTagValidatorTest.java
@@ -1,6 +1,6 @@
 /*
  * SteVe - SteckdosenVerwaltung - https://github.com/steve-community/steve
- * Copyright (C) 2013-2019 RWTH Aachen University - Information Systems - Intelligent Distributed Systems Group (IDSG).
+ * Copyright (C) 2013-2021 RWTH Aachen University - Information Systems - Intelligent Distributed Systems Group (IDSG).
  * All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify

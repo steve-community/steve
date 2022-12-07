9ad5abf48d949fe8edfced44890f882c6cff2c7c
Sevket Gökay <sevketgokay@gmail.com>
2022-12-07 10:12:02 +0100

    improve description of fields (#910)

--- src/main/java/de/rwth/idsg/steve/web/dto/QueryForm.java
+++ src/main/java/de/rwth/idsg/steve/web/dto/QueryForm.java
@@ -43,10 +43,10 @@ public abstract class QueryForm {
     @ApiModelProperty(value = "The OCPP tag")
     private String ocppIdTag;
 
-    @ApiModelProperty(value = "The start of the query time period. A date/time without timezone. Example: 2022-10-10T09:00")
+    @ApiModelProperty(value = "Show results that happened after this date/time. Format: ISO8601 without timezone. Example: `2022-10-10T09:00:00`")
     private LocalDateTime from;
 
-    @ApiModelProperty(value = "The end of the query time period. A date/time without timezone. Example: 2022-10-10T12:00")
+    @ApiModelProperty(value = "Show results that happened before this date/time. Format: ISO8601 without timezone. Example: `2022-10-10T12:00:00`")
     private LocalDateTime to;
 
     @ApiModelProperty(hidden = true)

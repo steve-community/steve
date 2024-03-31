0d59cb57dddaa3082c6aa9049bc281029a1fe545
mahmudarslan <mahmudarslan@gmail.com>
2024-03-31 03:02:13 +0300

    V0_8_6__update.sql bug fixed

--- src/main/resources/db/migration/V0_8_6__update.sql
+++ src/main/resources/db/migration/V0_8_6__update.sql
@@ -19,20 +19,64 @@ LOCK TABLES
   reservation WRITE,
   settings WRITE,
   `transaction` WRITE,
-  `user` WRITE;
+  `user` WRITE,
+  `schema_version` WRITE; 
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-ALTER TABLE charge_box CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE address CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE connector CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE connector_meter_value CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE connector_status CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE ocpp_tag CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE reservation CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE settings CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE `transaction` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE `user` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE settings CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE connector_status CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE connector_meter_value CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE address CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+ALTER TABLE `transaction` 
+DROP FOREIGN KEY `FK_transaction_ocpp_tag_id_tag` ,
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+ALTER TABLE reservation 
+DROP FOREIGN KEY `FK_reservation_charge_box_cbid` ,
+DROP FOREIGN KEY `FK_reservation_ocpp_tag_id_tag`,
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+ALTER TABLE ocpp_tag 
+DROP FOREIGN KEY `FK_ocpp_tag_parent_id_tag` ,
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+ALTER TABLE connector 
+DROP FOREIGN KEY `FK_connector_charge_box_cbid` ,
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+ALTER TABLE charge_box  CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+
+ALTER TABLE `transaction`
+ADD CONSTRAINT `FK_transaction_ocpp_tag_id_tag`
+FOREIGN KEY (`id_tag`)
+REFERENCES `ocpp_tag` (`id_tag`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+ 
+ALTER TABLE `reservation`
+ADD CONSTRAINT `FK_reservation_charge_box_cbid`
+FOREIGN KEY (`charge_box_id`)
+REFERENCES `charge_box` (`charge_box_id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+ 
+ALTER TABLE `ocpp_tag`
+ADD CONSTRAINT `FK_ocpp_tag_parent_id_tag`
+FOREIGN KEY (`parent_id_tag`)
+REFERENCES `ocpp_tag` (`id_tag`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+ 
+ALTER TABLE `connector`
+ADD CONSTRAINT `FK_connector_charge_box_cbid`
+FOREIGN KEY (`charge_box_id`)
+REFERENCES `charge_box` (`charge_box_id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
@@ -48,7 +92,8 @@ REPAIR TABLE
   reservation,
   settings,
   `transaction`,
-  `user`;
+  `user`,
+ `schema_version`;
 
 OPTIMIZE TABLE
   address,
@@ -60,4 +105,5 @@ OPTIMIZE TABLE
   reservation,
   settings,
   `transaction`,
-  `user`;
+  `user`,
+ `schema_version`;

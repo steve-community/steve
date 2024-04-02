19502ab16726fee9fe0eecdb94a9c76839904c91
Sevket Gökay <sevketgokay@gmail.com>
2024-04-02 23:28:03 +0200

    replace the view transaction (#1219)

--- src/main/resources/db/migration/V1_0_5__update.sql
+++ src/main/resources/db/migration/V1_0_5__update.sql
@@ -15,3 +15,21 @@ group by
     `o`.`expiry_date`,
     `o`.`max_active_transaction_count`,
     `o`.`note`;
+
+CREATE OR REPLACE VIEW `transaction` AS
+SELECT
+    tx1.transaction_pk,
+    tx1.connector_pk,
+    tx1.id_tag,
+    tx1.event_timestamp as 'start_event_timestamp',
+    tx1.start_timestamp,
+    tx1.start_value,
+    tx2.event_actor as 'stop_event_actor',
+    tx2.event_timestamp as 'stop_event_timestamp',
+    tx2.stop_timestamp,
+    tx2.stop_value,
+    tx2.stop_reason
+FROM transaction_start tx1
+LEFT JOIN transaction_stop tx2
+    ON tx1.transaction_pk = tx2.transaction_pk
+    AND tx2.event_timestamp = (SELECT MAX(event_timestamp) FROM transaction_stop s2 WHERE tx2.transaction_pk = s2.transaction_pk)

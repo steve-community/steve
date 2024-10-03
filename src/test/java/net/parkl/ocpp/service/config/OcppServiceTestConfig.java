80d37a68a0b127d0b23b2e5f8e00c7d88babdc0e
Andor Tóth <31831272+andortoth@users.noreply.github.com>
2024-10-03 11:30:30 +0200

    test fixes

--- src/test/java/net/parkl/ocpp/service/config/OcppServiceTestConfig.java
+++ src/test/java/net/parkl/ocpp/service/config/OcppServiceTestConfig.java
@@ -1,5 +1,6 @@
 package net.parkl.ocpp.service.config;
 
+import de.rwth.idsg.steve.SteveConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 
 @Configuration
-@ComponentScan(basePackages = {"net.parkl.ocpp","de.rwth.idsg.steve.repository","de.rwth.idsg.steve.ocpp.ws.cluster"})
+@ComponentScan(basePackages = {"net.parkl.ocpp","de.rwth.idsg.steve.repository","de.rwth.idsg.steve.ocpp","de.rwth.idsg.steve.ocpp.ws.cluster"})
 @Import({ JpaTestConfig.class, OcppServiceTestAdditionalConfig.class})
 public class OcppServiceTestConfig {
 	
@@ -27,6 +28,10 @@ public class OcppServiceTestConfig {
 	public ScheduledThreadPoolExecutor scheduledThreadPoolExecutor() {
 		return new ScheduledThreadPoolExecutor(10);
 	}
-	
+
+	@Bean
+	public SteveConfiguration steveConfiguration() {
+		return new SteveConfiguration();
+	}
 	
 }

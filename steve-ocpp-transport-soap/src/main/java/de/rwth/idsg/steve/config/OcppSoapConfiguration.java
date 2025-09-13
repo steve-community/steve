2cd110653e7f7e6542c255f897a4fa7d8f24dbc1
Julien Herr <jherr@bornerecharge.fr>
2025-09-13 17:23:49 +0200

    refactor: replace LoggingFeatureProxy by a bean

--- steve-ocpp-transport-soap/src/main/java/de/rwth/idsg/steve/config/OcppSoapConfiguration.java
+++ steve-ocpp-transport-soap/src/main/java/de/rwth/idsg/steve/config/OcppSoapConfiguration.java
@@ -19,7 +19,6 @@
 package de.rwth.idsg.steve.config;
 
 import de.rwth.idsg.steve.SteveConfiguration;
-import de.rwth.idsg.steve.ocpp.soap.LoggingFeatureProxy;
 import de.rwth.idsg.steve.ocpp.soap.MediatorInInterceptor;
 import de.rwth.idsg.steve.ocpp.soap.MessageIdInterceptor;
 import ocpp.cs._2010._08.CentralSystemService;
@@ -27,6 +26,9 @@ import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.common.logging.Slf4jLogger;
+import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.ext.logging.event.LogEvent;
+import org.apache.cxf.ext.logging.slf4j.Slf4jEventSender;
 import org.apache.cxf.feature.Feature;
 import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
@@ -63,28 +65,25 @@ public class OcppSoapConfiguration {
     private final ocpp.cs._2015._10.CentralSystemService ocpp16Server;
 
     private final PhaseInterceptor<Message> messageHeaderInterceptor;
-    private final LoggingFeatureProxy loggingFeatureProxy;
 
     public OcppSoapConfiguration(
             CentralSystemService ocpp12Server,
             ocpp.cs._2012._06.CentralSystemService ocpp15Server,
             ocpp.cs._2015._10.CentralSystemService ocpp16Server,
             @Qualifier("messageHeaderInterceptor") PhaseInterceptor<Message> messageHeaderInterceptor,
-            SteveConfiguration config,
-            LoggingFeatureProxy loggingFeatureProxy) {
+            SteveConfiguration config) {
         this.config = config;
         this.ocpp12Server = ocpp12Server;
         this.ocpp15Server = ocpp15Server;
         this.ocpp16Server = ocpp16Server;
         this.messageHeaderInterceptor = messageHeaderInterceptor;
-        this.loggingFeatureProxy = loggingFeatureProxy;
     }
 
     @PostConstruct
     public void init() {
         List<Interceptor<? extends Message>> interceptors =
                 asList(new MessageIdInterceptor(), messageHeaderInterceptor);
-        List<Feature> logging = singletonList(loggingFeatureProxy.get());
+        List<Feature> logging = singletonList(loggingFeature());
 
         createOcppService(ocpp12Server, "/CentralSystemServiceOCPP12", interceptors, logging);
         createOcppService(ocpp15Server, "/CentralSystemServiceOCPP15", interceptors, logging);
@@ -114,5 +113,26 @@ public class OcppSoapConfiguration {
         f.getFeatures().addAll(features);
         f.getInInterceptors().addAll(interceptors);
         f.create();
+    }
+
+    @Bean
+    public LoggingFeature loggingFeature() {
+        var feature = new LoggingFeature();
+        feature.setSender(new Slf4jEventSender() {
+            @Override
+            protected String getLogMessage(LogEvent event) {
+                var b = new StringBuilder();
+
+                b.append('\n') // Start from the next line to have the output well-aligned
+                        .append("    ExchangeId: ")
+                        .append(event.getExchangeId())
+                        .append('\n')
+                        .append("    Payload: ")
+                        .append(event.getPayload());
+
+                return b.toString();
+            }
+        });
+        return feature;
     }
 }
--- steve-ocpp-transport-soap/src/main/java/de/rwth/idsg/steve/ocpp/soap/ClientProvider.java
+++ steve-ocpp-transport-soap/src/main/java/de/rwth/idsg/steve/ocpp/soap/ClientProvider.java
@@ -22,6 +22,7 @@ import com.oneandone.compositejks.SslContextBuilder;
 import de.rwth.idsg.steve.SteveConfiguration;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
 import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.transport.http.HTTPConduit;
@@ -40,10 +41,10 @@ import jakarta.xml.ws.soap.SOAPBinding;
 public class ClientProvider {
 
     private final @Nullable TLSClientParameters tlsClientParams;
-    private final LoggingFeatureProxy loggingFeatureProxy;
+    private final LoggingFeature loggingFeature;
 
-    public ClientProvider(SteveConfiguration config, LoggingFeatureProxy loggingFeatureProxy) {
-        this.loggingFeatureProxy = loggingFeatureProxy;
+    public ClientProvider(SteveConfiguration config, LoggingFeature loggingFeature) {
+        this.loggingFeature = loggingFeature;
         var jettyConfig = config.getJetty();
         if (shouldInitSSL(jettyConfig)) {
             tlsClientParams = new TLSClientParameters();
@@ -54,7 +55,7 @@ public class ClientProvider {
     }
 
     public <T> T createClient(Class<T> clazz, String endpointAddress) {
-        var bean = getBean(endpointAddress);
+        var bean = createBean(endpointAddress);
         bean.setServiceClass(clazz);
         T clientObject = clazz.cast(bean.create());
 
@@ -67,10 +68,10 @@ public class ClientProvider {
         return clientObject;
     }
 
-    private JaxWsProxyFactoryBean getBean(String endpointAddress) {
+    private JaxWsProxyFactoryBean createBean(String endpointAddress) {
         var f = new JaxWsProxyFactoryBean();
         f.setBindingId(SOAPBinding.SOAP12HTTP_BINDING);
-        f.getFeatures().add(loggingFeatureProxy.get());
+        f.getFeatures().add(loggingFeature);
         f.getFeatures().add(new WSAddressingFeature());
         f.setAddress(endpointAddress);
         return f;

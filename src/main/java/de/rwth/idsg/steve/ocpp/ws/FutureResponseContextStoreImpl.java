e2b2aa65fdba4e5750fadc0b7c41e0741635aa19
Sevket Gökay <sevketgokay@gmail.com>
2026-02-19 16:02:58 +0100

    refactor: simplify by removing the dedicated RemoveFunction

--- src/main/java/de/rwth/idsg/steve/ocpp/ws/FutureResponseContextStoreImpl.java
+++ src/main/java/de/rwth/idsg/steve/ocpp/ws/FutureResponseContextStoreImpl.java
@@ -19,8 +19,6 @@
 package de.rwth.idsg.steve.ocpp.ws;
 
 import de.rwth.idsg.steve.ocpp.ws.data.FutureResponseContext;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Service;
@@ -28,7 +26,6 @@ import org.springframework.web.socket.WebSocketSession;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.BiFunction;
 
 /**
  * Presumption: The responses must be sent using the same connection as the requests!
@@ -57,7 +54,7 @@ public class FutureResponseContextStoreImpl implements FutureResponseContextStor
 
     @Override
     public void add(WebSocketSession session, String messageId, FutureResponseContext context) {
-        Map<String, FutureResponseContext> map = addIfAbsent(session);
+        var map = addIfAbsent(session);
         map.put(messageId, context);
         log.debug("Store size for sessionId '{}': {}", session.getId(), map.size());
     }
@@ -65,9 +62,13 @@ public class FutureResponseContextStoreImpl implements FutureResponseContextStor
     @Nullable
     @Override
     public FutureResponseContext get(WebSocketSession session, String messageId) {
-        RemoveFunction removeFunction = new RemoveFunction(messageId);
-        lookupTable.computeIfPresent(session, removeFunction);
-        return removeFunction.removedContext;
+        var map = lookupTable.get(session);
+        if (map == null) {
+            return null;
+        }
+        FutureResponseContext removedContext = map.remove(messageId);
+        log.debug("Store size for sessionId '{}': {}", session.getId(), map.size());
+        return removedContext;
     }
 
     private Map<String, FutureResponseContext> addIfAbsent(WebSocketSession session) {
@@ -75,21 +76,5 @@ public class FutureResponseContextStoreImpl implements FutureResponseContextStor
             log.debug("Creating new store for sessionId '{}'", innerSession.getId());
             return new ConcurrentHashMap<>();
         });
-    }
-
-    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-    private static class RemoveFunction implements
-            BiFunction<WebSocketSession, Map<String, FutureResponseContext>, Map<String, FutureResponseContext>> {
-
-        private final String messageId;
-        @Nullable private FutureResponseContext removedContext;
-
-        @Override
-        public Map<String, FutureResponseContext> apply(WebSocketSession session,
-                                                        Map<String, FutureResponseContext> map) {
-            removedContext = map.remove(messageId);
-            log.debug("Store size for sessionId '{}': {}", session.getId(), map.size());
-            return map;
-        }
     }
 }

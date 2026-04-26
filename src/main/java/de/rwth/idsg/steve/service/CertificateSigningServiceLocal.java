4fc647564306ec09ad44c2820e8dd0e17f279938
Sevket Gökay <sevketgokay@gmail.com>
2026-04-26 13:56:35 +0200

    nits

--- src/main/java/de/rwth/idsg/steve/service/CertificateSigningServiceLocal.java
+++ src/main/java/de/rwth/idsg/steve/service/CertificateSigningServiceLocal.java
@@ -37,6 +37,7 @@ import org.bouncycastle.asn1.x509.KeyUsage;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
@@ -221,7 +222,10 @@ public class CertificateSigningServiceLocal implements CertificateSigningService
     }
 
     private X509Certificate signCertificate(PKCS10CertificationRequest csr) throws Exception {
-        var issuer = new X500Name(caCertificate.getSubjectX500Principal().getName());
+        // Preserve exact issuer DN encoding from the configured CA certificate.
+        // Rebuilding from String can alter RDN ordering/encoding and break issuer matching in TLS.
+        // https://stackoverflow.com/a/12645265
+        var issuer = new JcaX509CertificateHolder(caCertificate).getSubject();
         var serial = new BigInteger(64, secureRandom);
 
         Date now = DateTime.now().toDate();

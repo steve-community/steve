fc615d5e3d5c5ebac5f4ee8098acf83fb69dbbe1
Sevket Gökay <sevketgokay@gmail.com>
2026-06-01 21:44:43 +0200

    introduce GroupedOpenApi for API docs
    in order to support multiple OpenAPI definitions. this commit effectively
    changes the path at which the spec is to /manager/v3/api-docs/admin

--- src/main/java/de/rwth/idsg/steve/config/ApiDocsConfiguration.java
+++ src/main/java/de/rwth/idsg/steve/config/ApiDocsConfiguration.java
@@ -30,11 +30,10 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,17 +47,22 @@ import java.util.List;
 @Configuration
 public class ApiDocsConfiguration {
 
+    public static final String API_DOCS_PATH = "/manager/v3/api-docs";
+
     public static final String ConfigurationKeyEnum_Read_Keys = "ConfigurationKeyEnum_Read_Keys";
     public static final String ConfigurationKeyEnum_Write_Keys = "ConfigurationKeyEnum_Write_Keys";
+
+    private static final String TITLE = "SteVe REST API Documentation";
+    private static final String[] API_PACKAGES = new String[]{
+        "de.rwth.idsg.steve.web.api"
+    };
 
     static {
         // Set the path with prefix /manager to protect the documentation behind regular sign-in
         // Default is just /v3/api-docs
-        System.setProperty("springdoc.api-docs.path", "/manager/v3/api-docs");
+        System.setProperty("springdoc.api-docs.path", API_DOCS_PATH);
         // Same for swagger ui
         System.setProperty("springdoc.swagger-ui.path", "/manager/swagger-ui/index.html");
-        // We only want REST APIs here (de.rwth.idsg.steve.web.api package)
-        System.setProperty("springdoc.paths-to-match", "/api/**");
         // Sort controllers alphabetically by their path
         System.setProperty("springdoc.swagger-ui.tagsSorter", "alpha");
         // Sort endpoints (within a controller) alphabetically by their path
@@ -67,10 +71,73 @@ public class ApiDocsConfiguration {
         System.setProperty("springdoc.writer-with-order-by-keys", "true");
     }
 
-    @Bean
-    public OpenAPI apiDocs(SteveProperties steveProperties) {
-        String title = "SteVe REST API Documentation";
+    /**
+     * Some OCPP operations' API payloads (e.g. {@link de.rwth.idsg.steve.web.dto.ocpp.ResetParams} are directly
+     * referencing OCPP enums (e.g. {@link ocpp.cp._2015._10.ResetType} that come with XML annotations. In order
+     * for Swagger/OpenAPI properly derive the correct enum values, we need to add XML binding introspector.
+     */
+    public ApiDocsConfiguration(ObjectMapperProvider objectMapperProvider) {
+        var mapper = objectMapperProvider.jsonMapper();
+        var existing = mapper.getSerializationConfig().getAnnotationIntrospector();
+        var xmlBindAnnotationIntrospector = new JakartaXmlBindAnnotationIntrospector(mapper.getTypeFactory());
+        mapper.setAnnotationIntrospector(AnnotationIntrospector.pair(existing, xmlBindAnnotationIntrospector));
+    }
 
+    @Bean
+    public GroupedOpenApi steveApi(SteveProperties steveProperties) {
+        return GroupedOpenApi.builder()
+            .group("admin")
+            .displayName("Admin")
+            .packagesToScan(API_PACKAGES)
+            .pathsToMatch("/api/**")
+            .addOpenApiCustomizer(steveApiCustomizer(steveProperties))
+            .build();
+    }
+
+    private static OpenApiCustomizer steveApiCustomizer(SteveProperties steveProperties) {
+        return openApi -> {
+            if (openApi.getComponents() == null) {
+                openApi.setComponents(new Components());
+            }
+
+            addInfo(openApi, steveProperties);
+            addAuthScheme(openApi);
+            addEnumConfSchemas(openApi);
+
+            // https://stackoverflow.com/a/68185254
+            openApi.setServers(List.of(new Server().url("/").description("Default Server URL")));
+        };
+    }
+
+    /**
+     * OCPP operations Get- and Change-Configuration allow different sets of keys. Not all keys are readable and
+     * writable. In order for OpenAPI spec to be correct in that regard, we specify them here.
+     */
+    private static void addEnumConfSchemas(OpenAPI openApi) {
+        openApi.getComponents().addSchemas(
+            ConfigurationKeyEnum_Read_Keys,
+            new StringSchema()._enum(new ArrayList<>(ConfigurationKeyEnum.READ))
+        );
+
+        openApi.getComponents().addSchemas(
+            ConfigurationKeyEnum_Write_Keys,
+            new StringSchema()._enum(new ArrayList<>(ConfigurationKeyEnum.WRITE))
+        );
+    }
+
+    private static void addInfo(OpenAPI openApi, SteveProperties steveProperties) {
+        var info = new Info()
+            .title(TITLE)
+            .license(new License()
+                .name("GPL-3.0")
+                .url("https://github.com/steve-community/steve/blob/master/LICENSE.txt")
+            )
+            .version(steveProperties.getVersion());
+
+        openApi.setInfo(info);
+    }
+
+    private static void addAuthScheme(OpenAPI openApi) {
         String securityName = "basicAuth";
 
         SecurityScheme securityScheme = new SecurityScheme()
@@ -78,56 +145,9 @@ public class ApiDocsConfiguration {
             .scheme("basic")
             .name(securityName);
 
-        return new OpenAPI()
-            .info(new Info()
-                .title(title)
-                .description(title)
-                .license(new License()
-                    .name("GPL-3.0")
-                    .url("https://github.com/steve-community/steve/blob/master/LICENSE.txt")
-                )
-                .version(steveProperties.getVersion())
-            )
-            // https://stackoverflow.com/a/68185254
-            .servers(List.of(new Server().url("/").description("Default Server URL")))
-            // define a security schema
-            .components(new Components().addSecuritySchemes(securityName, securityScheme))
-            // and activate it for all endpoints
-            .addSecurityItem(new SecurityRequirement().addList(securityName));
-    }
-
-    /**
-     * 1. Some OCPP operations' API payloads (e.g. {@link de.rwth.idsg.steve.web.dto.ocpp.ResetParams} are directly
-     * referencing OCPP enums (e.g. {@link ocpp.cp._2015._10.ResetType} that come with XML annotations. In order
-     * for Swagger/OpenAPI properly derive the correct enum values, we need to add XML binding introspector.
-     *
-     * 2. OCPP operations Get- and Change-Configuration allow different sets of keys. Not all keys are readable and
-     * writable. In order for OpenAPI spec to be correct in that regard, we specify them here.
-     */
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public OpenApiCustomizer openApiCustomizer(ObjectMapperProvider objectMapperProvider) {
-        {
-            var mapper = objectMapperProvider.jsonMapper();
-            var existing = mapper.getSerializationConfig().getAnnotationIntrospector();
-            var xmlBindAnnotationIntrospector = new JakartaXmlBindAnnotationIntrospector(mapper.getTypeFactory());
-            mapper.setAnnotationIntrospector(AnnotationIntrospector.pair(existing, xmlBindAnnotationIntrospector));
-        }
-
-        return openApi -> {
-            Components components = openApi.getComponents();
-
-            {
-                var schema = new StringSchema();
-                schema.setEnum(new ArrayList<>(ConfigurationKeyEnum.READ));
-                components.addSchemas(ConfigurationKeyEnum_Read_Keys, schema);
-            }
-
-            {
-                var schema = new StringSchema();
-                schema.setEnum(new ArrayList<>(ConfigurationKeyEnum.WRITE));
-                components.addSchemas(ConfigurationKeyEnum_Write_Keys, schema);
-            }
-        };
+        // define a security schema
+        openApi.getComponents().addSecuritySchemes(securityName, securityScheme);
+        // and activate it for all endpoints
+        openApi.addSecurityItem(new SecurityRequirement().addList(securityName));
     }
 }
